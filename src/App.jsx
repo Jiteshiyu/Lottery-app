6c86937b0784d3ab89c672086451fab9e7c3bbe8
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:37:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { sum } from "./helper.js";
 import Lottery from "./components/Lottery.jsx";
 
 function App() {
+  // Winning condition
   let winCondition = (ticket) => {
     return sum(ticket) === 15;
-    // return ticket.every((num) => num === ticket[0]);
+    // return ticket.every((num) => num === ticket[0]); // Alternative condition
   };
 
   return (
     <>
-      <Lottery ticketSize={3} winCondition={winCondition}></Lottery>
+      <Lottery ticketSize={3} winCondition={winCondition} /> {/* Render Lottery component */}
     </>
   );
 }

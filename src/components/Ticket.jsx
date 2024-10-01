5f5093504a89f6348b614f52e4e592c592c76b9a
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-02 01:12:46 +0530

    Update Ticket.jsx

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,10 +2,11 @@ import TicketNum from "./TicketNum.jsx";
 
 export default function Ticket({ ticket }) {
   return (
+    // Render each ticket number
     <p className="ticket">
       Ticket :{" "}
       {ticket.map((num, idx) => (
-        <TicketNum num={num} key={idx} /> // Render each ticket number
+        <TicketNum num={num} key={idx} /> 
       ))}
     </p>
   );
